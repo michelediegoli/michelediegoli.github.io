390763ad0de9efa99411ada5839fa6a06dc0078b
Michele <morticcino@diegoli.org>
2021-01-06 17:34:30 +0100

    fix actions stack

---
+++ Utils/js/mod/actions-stack.js
@@ -0,0 +1,25 @@
+var actionsStack = class{
+	constructor(){
+		this.actions = [];
+		this.index = 0;
+	}
+	addAction(type,data){
+		this.actions.push({t:type,d:data});
+		this.index = this.actions.length;
+	}
+	getLastAction(){
+		if(this.index>0){
+			this.index--;
+			return this.actions[this.index];
+		}else{
+			return null;
+		}
+	}
+	getNextAction(){
+		this.index++;
+		if(this.actions.length > this.index) return this.actions[this.index];
+		else return null;
+	}
+}
+
+export default actionsStack;
